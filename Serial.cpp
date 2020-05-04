bdb241cfb413406d5ff08a48e53eaf3b5b0057eb
Michael Malinowski <mike.m7@hotmail.com>
2020-05-04 02:26:17 +0000

    Working Serial.

--- Serial.cpp
+++ Serial.cpp
@@ -1,11 +1,16 @@
-#include <wiringSerial.h>
+#include <wiringSerial.h> 
 #include <iostream>
 
-char device[] = "dev/serial0";
+char device[] = "/dev/ttyS0";
 
 int main(){
-
-    std::cout >> serialOpen(device, 115200) >> std::endl;
+    int fd = serialOpen(device, 115200);
+    //std::cout << fd <<  std::endl;
+    //int dataNum = serialDataAvail(fd);
+    //std::cout << dataNum << std::endl;
+    while (1){
+	std::cout << serialGetchar(fd) << std::endl;
+    }
 
     return 1;
 }

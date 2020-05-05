335934c6d19c865caa03793e652f3e85847a09c5
Michael Malinowski <mike.m7@hotmail.com>
2020-05-04 20:57:42 -0400

    Testing serial multithreading

--- Serial.cpp
+++ Serial.cpp
@@ -1,16 +1,42 @@
 #include <wiringSerial.h> 
+#include <thread>
+#include <mutex>
 #include <iostream>
 
-char device[] = "/dev/ttyS0";
+//char device[] = "/dev/ttyS0";
+int serialRead = 1;
+int serialWrite = 1;
+
+int read(int fd){
+    while(serialRead){
+        std::cout << serialGetchar(fd) << std::endl;
+    }
+    return 0;
+}
+
+int write(){
+    for(int i = 0; i < 1000; ++i){
+        std::cout << "Writing: " << i << std::endl; 
+    }
+    return 0;
+}
 
 int main(){
     int fd = serialOpen(device, 115200);
-    //std::cout << fd <<  std::endl;
-    //int dataNum = serialDataAvail(fd);
-    //std::cout << dataNum << std::endl;
-    while (1){
-	std::cout << serialGetchar(fd) << std::endl;
-    }
-
-    return 1;
+    std::thread th1 (read, fd);
+    std::thread th2 (write);
 }
+
+
+
+// int main(){
+//     int fd = serialOpen(device, 115200);
+//     //std::cout << fd <<  std::endl;
+//     //int dataNum = serialDataAvail(fd);
+//     //std::cout << dataNum << std::endl;
+//     while (1){
+// 	std::cout << serialGetchar(fd) << std::endl;
+//     }
+
+//     return 1;
+// }
